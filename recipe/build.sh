d38c95bd7d40b73431cb8692da9b55868d19456b
Stefano Carrazza <stefano.carrazza@cern.ch>
2022-02-14 16:19:31 +0100

    updating recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# install the command-line interface
+cargo install --path pineappl_cli
+
 # install pineappl rust library with c-api support
 cargo install cargo-c
 cd pineappl_capi
 cargo cinstall --release --prefix=$PREFIX
+cd ..
 
-# install python wrapper
-cd ../wrappers/python
-pip install . -vv
+# install the python bindings
+cd pineappl_py
+maturin build --release
+cd pineappl_py/target/wheels/
+PYTHONDONTWRITEBYTECODE=1 PIP_CONFIG_FILE=/dev/null pip install --isolated --ignore-installed --no-deps *.whl
