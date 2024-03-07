26760f7c082c0224a15b30803c8809654d55fb10
juacrumar <juacrumar@lairen.eu>
2024-03-07 16:36:59 +0100

    added apple sillicon build ; rerendered with conda smith

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# install pineappl rust library with c-api support
-cargo install cargo-c --version 0.9.14+cargo-0.66
-cd pineappl_capi
-cargo cinstall --release --prefix=$PREFIX
-cd ..
+
+if [[ $HOST == *arm64-apple* ]]; then
+
+    curl --proto '=https' --tlsv1.2 -sSf https://nnpdf.github.io/pineappl/install-capi.sh | sh -s -- --prefix ${PREFIX} --target aarch64-apple-darwin
+
+else
+    # install pineappl rust library with c-api support
+    cargo install cargo-c --version 0.9.14+cargo-0.66
+    cd pineappl_capi
+    cargo cinstall --release --prefix=$PREFIX
+    cd ..
+fi
 
 # install the python bindings
 cd pineappl_py
