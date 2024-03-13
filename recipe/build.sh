0e4e02135f3e85fefafcc7d4a27a84cc94aad7aa
Christopher Schwan <handgranaten-herbert@posteo.de>
2024-03-13 11:10:57 +0100

    Use precompiled CAPI from github release page

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,15 +2,9 @@
 
 
 if [[ $HOST == *arm64-apple* ]]; then
-
     curl --proto '=https' --tlsv1.2 -sSf https://nnpdf.github.io/pineappl/install-capi.sh | sh -s -- --prefix ${PREFIX} --target aarch64-apple-darwin
-
 else
-    # install pineappl rust library with c-api support
-    cargo install cargo-c --version 0.9.14+cargo-0.66
-    cd pineappl_capi
-    cargo cinstall --release --prefix=$PREFIX
-    cd ..
+    curl --proto '=https' --tlsv1.2 -sSf https://nnpdf.github.io/pineappl/install-capi.sh | sh -s -- --prefix ${PREFIX}
 fi
 
 # install the python bindings
