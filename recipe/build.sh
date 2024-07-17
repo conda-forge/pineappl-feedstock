13cdbf6684886ada7679897dd5f8b2ab4d99384a
Christopher Schwan <handgranaten-herbert@posteo.de>
2024-07-17 09:48:20 +0200

    Revert commits

---
+++ recipe/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# install the CAPI
+curl --proto '=https' --tlsv1.2 -sSf https://nnpdf.github.io/pineappl/install-capi.sh | sh -s -- --prefix ${PREFIX}
+
+# install the python bindings
+cd pineappl_py
+maturin build --release --interpreter $PYTHON
+cd ../target/wheels/
+PYTHONDONTWRITEBYTECODE=1 PIP_CONFIG_FILE=/dev/null pip install --isolated --ignore-installed --no-deps *.whl
